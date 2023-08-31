ff426a9fa72863d046e73fd5b2979905cd791e74
damwead <mikhal3vsky@gmail.com>
2023-08-31 15:47:36 +0200

    ssr user profile page

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,9 +1,39 @@
 import UserProfile from "../../components/UserProfile";
 import PostFeed from "../../components/PostFeed";
 
-export async function getServerSideProps({ query }) {
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+import { query, collection, where, getDocs, limit, orderBy, getFirestore } from 'firebase/firestore';
 
 
+export async function getServerSideProps({ query: urlQuery }) {
+  // it'll be run automaticly on server
+
+  const { username } = urlQuery;
+
+  const userDoc = await getUserWithUsername(username);
+
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
+
+  // We need to retrive user posts
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+
+    const postsQuery = query(
+      collection(getFirestore(), userDoc.ref.path, 'posts'),
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'), // descend order
+      limit(5)
+    );
+    posts = (await getDocs(postsQuery)).docs.map(postToJSON); // need to be serializable to JSON
+  }
 
   return {
     props: { user, posts}, 
---
+++ pages/admin/index.js
@@ -0,0 +1,9 @@
+
+
+export default function AdminPage() {
+  return(
+    <main class="container">
+      <h1>Admin</h1>
+    </main>
+  )
+}
